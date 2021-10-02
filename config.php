e5464cab5e56aaa959641e55d6f0a627b85bf639
vadlamuricloudops <88264138+vadlamuricloudops@users.noreply.github.com>
2021-10-02 18:13:29 +0530

    Update config.php

--- config.php
+++ config.php
@@ -8,10 +8,10 @@ $email    = "";
 $errors = array(); 
 
 // connect to the database
-define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'PuneethReddy');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'ecommerece');
+define('DB_SERVER', 'wordpress.cs6royptlt8t.us-west-2.rds.amazonaws.com');
+   define('DB_USERNAME', 'wordpress');
+   define('DB_PASSWORD', 'wordpress123');
+   define('DB_DATABASE', 'ecommerse');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 if (!$db) {
     die("Connection failed: " . mysqli_connect_error());
